7b389f22d7e5de62fcc5690766f653faa1a533fb
0xalfaa <callmealfaa@gmail.com>
2024-06-04 11:10:49 +0800

    Tambah update jumlah keranjang

--- application/views/pesanan.php
+++ application/views/pesanan.php
@@ -1,12 +1,13 @@
 <div class="container-fluid">
     <h4>Keranjang Belanja</h4>
-    <table class="table table-bordered table-striped table-hover">
+    <table class="table table-striped">
         <tr>
             <th>NO</th>
-            <th>Nama Menu</th>
-            <th>Jumlah</th>
-            <th>Harga</th>
-            <th>Subtotal</th>
+            <th width="30%">Nama Menu</th>
+            <th width="13%">Jumlah</th>
+            <th width="15%">Harga</th>
+            <th width="20%" class="text-right">Subtotal</th>
+            <th width="12%"></th>
         </tr>
 
         <?php
@@ -15,21 +16,38 @@
         <tr>
             <td><?php echo $no++ ?></td>
             <td><?php echo $items['name'] ?></td>
-            <td><?php echo $items['qty'] ?></td>
+            <td><input type="number" class="form-control text-center" value="<?php echo $items["qty"]; ?>" onchange="update_item_keranjang(this, '<?php echo $items["rowid"]; ?>')"></td>
             <td align="right">Rp. <?php echo number_format($items['price'], 0,',','.') ?></td>
             <td align="right">Rp. <?php echo number_format($items['subtotal'], 0,',','.') ?></td>
+            <td align="center" width="20%" onclick="return confirm('yakin ingin menghapus keranjang?')"><?php echo anchor('dashboard/hapus_item_keranjang/'.$items['rowid'],'<div class="btn-delete"><i class="fas fa-lg fa-times"></i></div>')?></td>
         </tr>
         <?php  endforeach; ?>
         <tr>
             <td colspan="4"></td>
+            <td><strong>Total</strong></td>
             <td align="right">
                 Rp.  <?php echo number_format($this->cart->total(), 0,',','.') ?>
             </td>
         </tr>
     </table>
     <div align="right">
-       <a href="<?php echo base_url('dashboard/hapus_pesanan');?>"> <div class="btn btn-danger btn-sm">Hapus Pesanan</div></a> 
+       <!-- <a href="<?php echo base_url('dashboard/hapus_pesanan');?>"> <div class="btn btn-danger btn-sm">Hapus Pesanan</div></a>  -->
        <a href="<?php echo base_url('dashboard/');?>"> <div class="btn btn-primary btn-sm">Pesan lagi</div></a> 
        <a href="<?php echo base_url('dashboard/pembayaran');?>"> <div class="btn btn-success btn-sm">Pembayaran</div></a> 
     </div>
 </div>
+<!-- Include jQuery library -->
+<script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+
+<script>
+// Update item quantity
+function update_item_keranjang(obj, rowid){
+    $.get("<?php echo base_url('dashboard/update_jumlah_menu/'); ?>", {rowid:rowid, qty:obj.value}, function(resp){
+        if(resp == 'ok'){
+            location.reload();
+        }else{
+            alert('Cart update failed, please try again.');
+        }
+    });
+}
+</script>
